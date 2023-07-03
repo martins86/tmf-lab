973900c3a6408b9e3d0653dd5de4f9bbaa1f1ee9
martins86 <martins86@gmail.com>
2023-07-03 20:41:59 +0000

    feat: iniciando translate

--- projects/portal/src/app/app.module.ts
+++ projects/portal/src/app/app.module.ts
@@ -1,6 +1,11 @@
+import { APP_BASE_HREF } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -12,10 +17,27 @@ import { DefaultModule } from './layout/default/default.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     BrowserAnimationsModule,
     DefaultModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function HttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json')
+}
