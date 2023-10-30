80ff9e2c416c3e8fc3f5cb5badf89419711f03a6
martins86 <martins86@gmail.com>
2023-10-30 22:10:08 +0000

    feat: modulo de maintenance pagina em manutencao

---
+++ projects/portal/src/app/pages/maintenance/maintenance.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MaintenanceComponent } from './maintenance.component';
+
+describe('Testes do MaintenanceComponent', () => {
+  let component: MaintenanceComponent;
+  let fixture: ComponentFixture<MaintenanceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MaintenanceComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MaintenanceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Deve inicializar o MaintenanceComponent', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/portal/src/app/pages/maintenance/maintenance.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance',
+  templateUrl: './maintenance.component.html',
+  styleUrls: ['./maintenance.component.scss'],
+})
+export class MaintenanceComponent {}
