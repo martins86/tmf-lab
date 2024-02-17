78d0cfae407ac33a7e6aa2835de8d77dee9e1463
martins86 <martins86@gmail.com>
2024-02-17 22:13:17 +0000

    feat: ajustes e melhorias no form cadastro

--- projects/portal/src/app/pages/register/register.component.ts
+++ projects/portal/src/app/pages/register/register.component.ts
@@ -1,40 +1,71 @@
 import { Component } from '@angular/core';
 
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ISelectItem } from '@interfaces/iselect-item.interface';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  registerForm!: FormGroup;
-  controlName = new FormControl('', Validators.required);
-  controlEmail = new FormControl('', Validators.pattern(/.+@.+\..+/));
+	registerForm!: FormGroup;
+	controlName!: FormControl;
+	controlEmail!: FormControl;
+	controlNickname!: FormControl;
+	controlItem!: FormControl;
+	listItems!: ISelectItem[];
 
-  constructor(private formBuilder: FormBuilder) {
-    this.startRegisterForm();
-  }
+	constructor(private formBuilder: FormBuilder) {
+		this.startControlFields();
+		this.startRegisterForm();
+		this.getListItems();
+	}
 
-  startRegisterForm(): void {
-    this.registerForm = this.formBuilder.group({
-      fieldName: this.controlName,
-      fieldEmail: this.controlEmail,
-    });
-  }
+	getListItems(): void {
+		this.listItems = [];
 
-  onSubmit(): void {
-    console.log(this.registerForm.value);
-  }
+		if (this.listItems?.length < 1) {
+			this.controlItem.setValue('XX');
+			this.controlItem.markAsTouched();
+		}
+	}
 
-  getErrorEmailMessage(): string {
-    return this.controlEmail.hasError('required')
-      ? 'E-mail Obrigatório'
-      : 'E-mail inválido';
-  }
+	startControlFields(): void {
+		this.controlName = new FormControl('', Validators.required);
+		this.controlEmail = new FormControl('', Validators.pattern(/.+@.+\..+/));
+		this.controlNickname = new FormControl('');
+		this.controlItem = new FormControl('', [Validators.required, Validators.minLength(3)]);
+	}
+
+	startRegisterForm(): void {
+		this.registerForm = this.formBuilder.group({
+			fieldName: this.controlName,
+			fieldEmail: this.controlEmail,
+			fieldNickname: this.controlNickname,
+			fieldItem: this.controlItem,
+		});
+	}
+
+	onSubmit(): void {
+		if (this.registerForm.valid) {
+			console.log('Submit: ', this.registerForm.value);
+		}
+	}
+
+	resetForm(): void {
+		this.startControlFields();
+		this.getListItems();
+	}
+
+	getStyle(field: FormControl) {
+		return {
+			'mat-form-field-valid': field.valid && field.touched,
+		};
+	}
+
+	getErrorEmailMessage(): string {
+		return this.controlEmail.hasError('required') ? 'E-mail Obrigatório!' : 'E-mail inválido';
+	}
 }
--- projects/portal/src/app/pages/register/register.module.ts
+++ projects/portal/src/app/pages/register/register.module.ts
@@ -5,31 +5,21 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
-const materialModules = [
-  FormsModule,
-  ReactiveFormsModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule,
-];
+const materialModules = [FormsModule, ReactiveFormsModule, MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule];
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 
 @NgModule({
-  declarations: [RegisterComponent],
-  imports: [
-    CommonModule,
-    RegisterRoutingModule,
-    ...materialModules,
-    TranslateModule.forChild(),
-  ],
-  exports: [RegisterComponent],
+	declarations: [RegisterComponent],
+	imports: [CommonModule, RegisterRoutingModule, ...materialModules, TranslateModule.forChild()],
+	providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline', floatLabel: 'auto' } }],
+	exports: [RegisterComponent],
 })
 export class RegisterModule {}
