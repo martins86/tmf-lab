40b789af14155e976907633f88254ad22914bb7f
martins86 <martins86@gmail.com>
2024-02-17 13:16:28 +0000

    fix: testes dp register

--- projects/portal/src/app/pages/register/register.component.spec.ts
+++ projects/portal/src/app/pages/register/register.component.spec.ts
@@ -6,6 +6,7 @@ import { TranslateStubModule } from '@utils/stubs/translate-stub.module';
 import { RegisterComponent } from './register.component';
 import { RegisterModule } from './register.module';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Testes do RegisterComponent', () => {
   let component: RegisterComponent;
@@ -14,7 +15,12 @@ describe('Testes do RegisterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
-      imports: [RegisterModule, TranslateStubModule, RouterModule.forRoot([])],
+      imports: [
+        RegisterModule,
+        BrowserAnimationsModule,
+        TranslateStubModule,
+        RouterModule.forRoot([]),
+      ],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
